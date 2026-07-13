31fbe3108deaf3291d63ecf44b0a3995dd161db8
Pedro Franceschi <contatopedrovagas@gmail.com>
2026-07-13 17:52:34 -0300

    Special move - Promotion

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -47,12 +47,12 @@ public class Pawn extends ChessPiece {
 			if (position.getRow() == 3) {
 				Position left = new Position(position.getRow(), position.getColumn() - 1);
 				if (getBoard().positionExists(left) && isThereOpponentPiece(left)
-						&& getBoard().piece(left) == chessMatch.GetEnPassantVulnerable()) {
+						&& getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
 					mat[left.getRow() - 1][left.getColumn()] = true;
 				}
 				Position right = new Position(position.getRow(), position.getColumn() + 1);
 				if (getBoard().positionExists(right) && isThereOpponentPiece(right)
-						&& getBoard().piece(right) == chessMatch.GetEnPassantVulnerable()) {
+						&& getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
 					mat[right.getRow() - 1][right.getColumn()] = true;
 				}
 			}
@@ -82,12 +82,12 @@ public class Pawn extends ChessPiece {
 			if (position.getRow() == 4) {
 				Position left = new Position(position.getRow(), position.getColumn() - 1);
 				if (getBoard().positionExists(left) && isThereOpponentPiece(left)
-						&& getBoard().piece(left) == chessMatch.GetEnPassantVulnerable()) {
+						&& getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
 					mat[left.getRow() + 1][left.getColumn()] = true;
 				}
 				Position right = new Position(position.getRow(), position.getColumn() + 1);
 				if (getBoard().positionExists(right) && isThereOpponentPiece(right)
-						&& getBoard().piece(right) == chessMatch.GetEnPassantVulnerable()) {
+						&& getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
 					mat[right.getRow() + 1][right.getColumn()] = true;
 				}
 			}
